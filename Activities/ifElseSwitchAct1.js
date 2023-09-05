1d1f33390f3e1519f954b041d0d3923e565dd500
Joe Simpson <josephsimpson94@googlemail.com>
2023-09-05 10:14:32 +0100

    Age Checker Activity

---
+++ Activities/ifElseSwitchAct1.js
@@ -0,0 +1,7 @@
+let age = 16;
+let place = "UK";
+
+if (age == ">17" && place == "UK") {
+  console.log("Yes I can serve you.");
+} else age == "<=17" && place == "UK";
+console.log("You aren't old enough.");
